be15665bc0851264348be0073d867b82052a10e3
Nadin <nadinchik-mar@mail.ru>
2018-10-16 23:39:04 +0300

    Append

--- js/js.js
+++ js/js.js
@@ -1,29 +1,35 @@
-$(document).ready (function($) {
+$(document).ready(function ($) {
     addTask();
-    let todos =[];
-    let taskId=0;
-    let actualList=[];
+    let todos = [];
+    let taskId = 0;
 
-    function appendTodo(){
-        let checked = actualList[index].status;
+    function appendTodo() {
         $('#todoList').find('li').remove();
-        $(todos).each(function(i,t) {
-            $('#todoList').append('<li class="todoItem '+ t.status +'"  id="${taskId}">\
-                <div class="todoTask"><input class="todoCheckbox" type="checkbox">\
-                <input class="textTask" type="text" readonly  value="'+ t.textTodo +'">\
-                <button class="delTodo" id="'+ i +'">x</button>\</div>\</li>');
+        console.log('todos -==> ', todos);
+        todos.forEach((item, index) => {
+            if(item.status){
+                $('#todoList').append(`<li class="todoItem todoItem_checked"  id={`${taskId}`}>`+
+                `<input class="todoCheckbox" type="checkbox" checked>`+
+                `<input class="textTask" type="text" readonly  value="' + item.textTodo + '">`+ `<button class="delTodo" id="' + index + '">x</button>`+ '</li>`);
+            }else{
+                $('#todoList').append(`<li class="todoItem todoItem_checked"  id={`${taskId}`}>`+
+                    `<input class="todoCheckbox" type="checkbox">`+
+                    `<input class="textTask" type="text" readonly  value="' + item.textTodo + '">`+ `<button class="delTodo" id="' + index + '">x</button>`+ '</li>`);
+            }
+
         });
-        if (checked==true) {
-            $('#' + taskId).find(':checkbox').prop('checked', true);
-        } else {
-            $('#' + taskId).find(':checkbox').prop('checked', false);
-        }
+        // if (checked==true) {
+        //     $('#' + taskId).find(':checkbox').prop('checked', true);
+        // } else {
+        //     $('#' + taskId).find(':checkbox').prop('checked', false);
+        // }
         todos.filter(function (task) {
             if (task.status == false) {
                 activeCount += 1;
-                console.log(activeCount)
+                console.log(activeCount);
             } else {
-                completedCount += 1;}
+                completedCount += 1;
+            }
         });
         $('#activeCount').text('Active: ' + activeCount);
         $('#completedCount').text('Completed: ' + completedCount);
@@ -41,16 +47,16 @@ $(document).ready (function($) {
     }
 
 
-
     function addTask() {
         $("#addBtn").click(function () {
             let valueTask = $("#todoInput").val();
             let todo = {
                 textTodo: valueTask,
                 taskId: Math.random(),
-                status: false,}
+                status: false,
+            };
             if (!(valueTask.trim())) {
-                alert("Enter text!")
+                alert("Enter text!");
             }
             $('#footer').show();
             todos.push(todo);
@@ -105,15 +111,15 @@ $(document).ready (function($) {
     $('#delCompleteTodo').on('click', delComplete);
 
 
-
-    function delComplete(){
+    function delComplete() {
         for (i = 0; i < todos.length; i++) {
             if (todos[i].status == true) {
-                todos.splice(todos.indexOf(todos[i]), 1)
-                --i}
+                todos.splice(todos.indexOf(todos[i]), 1);
+                --i;
+            }
         }
-        $('#allCheck').prop('checked', false)
-        filteredList = todos.slice()
+        $('#allCheck').prop('checked', false);
+        filteredList = todos.slice();
 
     }
 
@@ -122,15 +128,21 @@ $(document).ready (function($) {
     $('#todoList').on('click', '.delTodo', function () {
         const idForDelete = $(this).parent().attr("id");
         let indexForDeleteTodoArray;
-        todos.forEach((todo, i)=>{
-            if(todo.id===idForDelete){
-                indexForDeleteTodoArray=i;
+        todos.forEach((todo, i) => {
+            if (todo.id === idForDelete) {
+                indexForDeleteTodoArray = i;
             }
         });
-        todos.splice(indexForDeleteTodoArray, 1);
+        if (indexForDeleteTodoArray) {
+            todos.splice(indexForDeleteTodoArray, 1);
+        }
         $(this).parent().empty();
-        console.log(todos)
+        console.log(todos);
     });
+
+
+});
+
 
 
 
